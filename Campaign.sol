09e855ffcccb394b0d78c7ca6798aa0d4576d493
Wadah Esam <wadah-wleed@hotmail.com>
2018-09-21 23:33:18 +0300

    .

--- Campaign.sol
+++ Campaign.sol
@@ -4,15 +4,17 @@ contract Campaign {
 
     // this struct is creating a defintion only not a variable
     struct Request {
-        string description; // what is this money used for
-        uint value;         // the value that the manager is willing to send to the vendor
-        address recipient;  // the address of the vendor
-        bool complete;      // this will specify if the request has been finalized or not 
+        string description;                     // what is this money used for
+        uint value;                             // the value that the manager is willing to send to the vendor
+        address recipient;                      // the address of the vendor
+        bool complete;                          // this will specify if the request has been finalized or not 
+        mapping(address => bool) approvals;     // mapping to get if a certin address approved a request or not
+        uint approvalCount;                     // number of people who approved this request 
     }
         
     address public manager;
     uint public minimumContribution;
-    address[] public approvers;
+    mapping(address => bool) public approvers;
     Request[] public requests;
     
     modifier restrected() {
@@ -27,18 +29,25 @@ contract Campaign {
     
     function contribute() public payable {
         require(msg.value >= minimumContribution);
-        approvers.push(msg.sender);
+        
+        // adding a new mapping to approvers
+        // close to javascript objects 
+        approvers[msg.sender] = true; 
     }
     
     // called by the manager to create a request for buying something
     function createRequest(string description, uint value, address recipient) public restrected {
         
         // creating instance of the struct 
+        // notice that you don't have to initilize the approvals field
+        // and that is because we only have to initilize the value types
+        // and then we will be able to access the reference types 
         Request memory newRequest = Request({
             description: description,
             value: value,
             recipient: recipient,
-            complete: false
+            complete: false,
+            approvalCount: 0
         });
          
         // anther way of defining struct instances
